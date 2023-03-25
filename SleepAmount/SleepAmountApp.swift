c86a067353e44bd3d69c205ede63745bee75bc48
James Wilhelm <jrw0594@gmail.com>
2023-03-25 00:05:54 -0400

    Initial Commit

---
+++ SleepAmount/SleepAmountApp.swift
@@ -0,0 +1,17 @@
+//
+//  SleepAmountApp.swift
+//  SleepAmount
+//
+//  Created by James Wilhelm on 3/24/23.
+//
+
+import SwiftUI
+
+@main
+struct SleepAmountApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
